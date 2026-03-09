b7fca9c65974f2fbd64810ede3a3a1514d767bc2
Vitali Quiering <vitali@quiering.com>
2026-03-09 17:15:33 +0100

    Add ESLint config and cleanup Fastmail node
    Introduce new eslint.config.js with TypeScript support
    Remove embedded Fastmail icon data URL and iconUrl usage
    Simplify prepublishOnly script to only run build

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['nodes/**/*.ts', 'credentials/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {},
+  },
+  {
+    files: ['package.json'],
+    rules: {},
+  },
+];
